aa96c3618ab2a8f67c31cd83fa36efd50376f4b5
Hoeks, Maikel <maikelhoeks@hotmail.com>
2014-11-03 14:07:56 +0100

    pod en puck

--- Airhockey/src/game/Pod.java
+++ Airhockey/src/game/Pod.java
@@ -5,10 +5,67 @@
  */
 package game;
 
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+
 /**
  *
  * @author Joris
  */
 public class Pod {
+    //Game objects
+    private final IPlayer player;
+    
+    //Physics objects
+    public final Body body;
+    
+     /**
+     *
+     * @param world GameWorld object this Pod is child of.
+     * @param player The player this pod belongs to.
+     * @param startPosition The starting position of this pod.
+     */
+    public Pod(World world, IPlayer player, Vec2 startPosition) {
+        this.player = player;
+
+        //body definition
+        BodyDef bd = new BodyDef();
+        bd.position.set(startPosition.x, startPosition.y);
+        bd.type = BodyType.KINEMATIC;
+        //define shape of the body.
+        CircleShape cs = new CircleShape();
+        cs.m_radius = (float) world.getField().getPodSize() / 2;
+        //define fixture of the body.
+        FixtureDef fd = new FixtureDef();
+        fd.shape = cs;
+        fd.density = 0.5f;
+        fd.friction = 0.3f;
+        fd.restitution = 1f;
+        //create the body and add fixture to it
+        body = world.getPhysWorld().createBody(bd);
+        body.createFixture(fd);
+        body.setUserData(this);
+    }
+    
+     /**
+     *
+     * @return The player this object belongs to
+     */
+    public IPlayer getPlayer() {
+        return this.player;
+    }
+
+    /**
+     *
+     * @return The position of this object
+     */
+    public Vec2 getPosition() {
+        return body.getPosition();
+    }
+    
     
 }
