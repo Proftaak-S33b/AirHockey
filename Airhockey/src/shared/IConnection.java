dc9818f0c9164e360d7718266f4daa37dc2c14e4
Joris <jorisdouven@hotmail.com>
2014-11-05 10:53:11 +0100

    Fixed library reference

---
+++ Airhockey/src/shared/IConnection.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shared;
+
+/**
+ *
+ * @author Joris
+ */
+public class IConnection {
+    private float xRedPod;
+    private float yRedPod;
+    
+    public void setRedPod(float x, float y) {
+        xRedPod = x;
+        yRedPod = y;
+    }
+}
