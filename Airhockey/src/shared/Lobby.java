d61209d4b8a2f1c89491a803511fd7439d12cf97
Joris <jorisdouven@hotmail.com>
2014-11-05 12:59:17 +0100

    Moved Lobby

--- Airhockey/src/shared/Lobby.java
+++ Airhockey/src/shared/Lobby.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package game;
+package shared;
 
+import game.IPlayer;
 import java.util.ArrayList;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
