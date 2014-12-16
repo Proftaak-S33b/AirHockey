54505b402c63e3f98e423b5373889c55341fff3e
jur-clerkx <jur.c@live.nl>
2014-12-16 16:31:08 +0100

    Oeps...

--- Airhockey/src/controllers/DatabaseManager.java
+++ Airhockey/src/controllers/DatabaseManager.java
@@ -12,24 +12,24 @@ import networking.Player_Leaderboard;
 
 /**
  * Static class for the database connection.
+ *
  * @author Jur
  */
 public final class DatabaseManager {
+
     public static Connection connection;
-    
+
     /**
      * Opens the connection to the database
-     * @return Returns true if succesfull,
-     * else returns false
+     *
+     * @return Returns true if succesfull, else returns false
      */
-    private static boolean openConnection()
-    {
+    private static boolean openConnection() {
         boolean result;
-        try{
+        try {
             connection = DriverManager.getConnection("jdbc:mysql://a-chan.nl:3306/deb82648_pts", "deb82648_air", "airhockey");
             result = true;
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             connection = null;
             System.out.println(e.getMessage());
             System.out.println("Connection failed");
@@ -37,71 +37,69 @@ public final class DatabaseManager {
         }
         return result;
     }
-    
+
     /**
      * Closes the database connection
      */
-    private static void closeConnection(){
-        try{
+    private static void closeConnection() {
+        try {
             connection.close();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-    
+
     /**
      * Checks if the username and password the user entered are correct.
+     *
      * @param username The username of the user
      * @param password The password of the user
      * @return Returns a boolean if the password and username match or not
      */
-    public static boolean authenticateUser(String username, String password){
+    public static boolean authenticateUser(String username, String password) {
         boolean result = false;
         //Open connection
-        if(openConnection()){
-            try{
+        if (openConnection()) {
+            try {
                 //Try to execute sql statment
                 Statement stmnt = connection.createStatement();
                 String SQL = "SELECT playerName, playerPassword FROM PLAYER WHERE playerName = '";
-                SQL += username+ "'" + " AND playerPassword = " + "'" + password + "'" + ";";
+                SQL += username + "'" + " AND playerPassword = " + "'" + password + "'" + ";";
                 ResultSet rs = stmnt.executeQuery(SQL);
                 //Check if password and username match
-                if(rs.next())
-                {
+                if (rs.next()) {
                     result = true;
                 }
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
             closeConnection();
         }
         return result;
     }
-    
+
     /**
      * Creates a new user with the given variables
+     *
      * @param username Username for the user
      * @param password Password for the user
      * @return Boolean. True if succesfull, false if not
      */
-    public static boolean addUser(String username, String password){
+    public static boolean addUser(String username, String password) {
         boolean result = false;
         //Open the connection
-        if(openConnection() && !username.trim().isEmpty() && !password.trim().isEmpty()){
-            try{
+        if (openConnection() && !username.trim().isEmpty() && !password.trim().isEmpty()) {
+            try {
                 //Try to execute sql statment
                 Statement stmnt = connection.createStatement();
                 String SQL = "INSERT INTO PLAYER VALUES(";
                 SQL += "'" + username + "'";
                 SQL += ",'" + password + "'";
                 SQL += ", 0);";
-                if(stmnt.executeUpdate(SQL) > 0){
+                if (stmnt.executeUpdate(SQL) > 0) {
                     result = true;
                 }
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
@@ -109,49 +107,48 @@ public final class DatabaseManager {
         closeConnection();
         return result;
     }
-    
+
     /**
      * Gets the list of all players, ordered by score.
+     *
      * @return The table that was found
      */
-    public static ObservableList<Player_Leaderboard> getTopPlayers()
-    {
+    public static ObservableList<Player_Leaderboard> getTopPlayers() {
         ObservableList<Player_Leaderboard> result = FXCollections.observableArrayList();
         //Open connection
-        if(openConnection()){
-            try{
+        if (openConnection()) {
+            try {
                 //Try to execute sql statment
                 Statement stmnt = connection.createStatement();
-                String SQL = "SELECT playerName, playerRating FROM PLAYER ORDER BY playerRating DESC;"; 
+                String SQL = "SELECT playerName, playerRating FROM PLAYER ORDER BY playerRating DESC;";
                 ResultSet rs = stmnt.executeQuery(SQL);
                 //Put all the records in a table
                 int i = 0;
-                while(rs.next()){
+                while (rs.next()) {
                     i++;
                     result.add(new Player_Leaderboard(i, rs.getString(1), rs.getString(2)));
                 }
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
             closeConnection();
         }
         return result;
     }
-    
-    public static int getRanking(String PlayerName)
-    {
+
+    public static int getRanking(String PlayerName) {
         int result = -1;
         //Open connection
-        if(openConnection()){
-            try{
+        if (openConnection()) {
+            try {
                 //Try to execute sql statment
                 Statement stmnt = connection.createStatement();
-                String SQL = "SELECT playerRating FROM PLAYER WHERE playerName = '" + PlayerName + "';"; 
+                String SQL = "SELECT playerRating FROM PLAYER WHERE playerName = '" + PlayerName + "';";
                 ResultSet rs = stmnt.executeQuery(SQL);
-                result = rs.getInt(1);
-            }
-            catch(Exception e){
+                if (rs.next()) {
+                    result = rs.getInt(1);
+                }
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
             closeConnection();
