99e30dd08c1857688718d1ac167476581a12ccca
Joris <jorisdouven@hotmail.com>
2014-12-16 18:57:06 +0100

    RMIServer GUI

---
+++ Airhockey/src/networking/standalone/RMIServerController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package networking.standalone;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Joris
+ */
+public class RMIServerController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
