451b8494b9d0fee7b38151213842657f714f6f80
Joris <jorisdouven@hotmail.com>
2015-01-21 14:34:29 +0100

    Minor update

--- Airhockey/src/networking/commands/ServerReceiver.java
+++ Airhockey/src/networking/commands/ServerReceiver.java
@@ -15,25 +15,37 @@ import networking.standalone.Lobby;
  * @author Etienne
  */
 public class ServerReceiver {
-    
+
     private static int NEXTLOBBYID = 10000;
-    
+
     private List<Lobby> lobbyList;
-    
-    public ServerReceiver(){
+
+    public ServerReceiver() {
         lobbyList = new ArrayList<>();
     }
-    
-    public void executeCommand(Command command){
+
+    public void executeCommand(Command command) {
         //pass reference to command object
         command.Execute();
     }
-    
-    public void addLobby(String name, IPlayer host){
-        
+
+    public void addLobby(String name, IPlayer host) {
+
     }
-    
-    public void removeLobby(int id){
-        
+
+    public void removeLobby(int id) {
+
+    }
+
+    public void changeLobbyName(int id, String newName) {
+        for (Lobby l : lobbyList) {
+            if (l.getID() == id) {
+                l.setGameName(newName);
+            }
+        }
+    }
+
+    public void joinLobby(int id, IPlayer player) {
+
     }
 }
