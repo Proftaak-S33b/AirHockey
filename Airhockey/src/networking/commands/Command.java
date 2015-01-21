df83f4f264a8c269808f64a580e573c48ff35177
Etienne Cooijmans <etiennecooijmans@gmail.com>
2015-01-21 13:24:27 +0100

    Start of Command Pattern Implementation.
    Make sure to cast commands to the interface, not the object.

---
+++ Airhockey/src/networking/commands/Command.java
@@ -0,0 +1,13 @@
+package networking.commands;
+
+/**
+ * Command design pattern for sending commands over sockets.
+ * @author Etienne
+ */
+public interface Command {
+    
+    /**
+     * Whatever the command object wants to do is called from here.
+     */
+    public void Execute();    
+}
