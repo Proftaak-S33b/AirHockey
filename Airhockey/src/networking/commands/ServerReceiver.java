940ac0a7ae4ee2d5778318b00d0f065bab44db77
Hoeks, Maikel <maikelhoeks@hotmail.com>
2015-01-21 14:39:08 +0100

    javadoc

--- Airhockey/src/networking/commands/ServerReceiver.java
+++ Airhockey/src/networking/commands/ServerReceiver.java
@@ -23,32 +23,41 @@ public class ServerReceiver {
     public ServerReceiver() {
         lobbyList = new ArrayList<>();
     }
-    
+
     /**
-     * Exectuce the command 
+     * Exectuce the command
+     *
      * @param command The command to be executed
      */
-    public void executeCommand(Command command){
+    public void executeCommand(Command command) {
         //pass reference to command object
         command.Execute();
     }
-    
+
     /**
      * Add a lobby
+     *
      * @param name the name of the lobby
      * @param host the host of the lobby
      */
-    public void addLobby(String name, IPlayer host){
-        
+    public void addLobby(String name, IPlayer host) {
+
     }
-    
+
     /**
      * remove a lobby
+     *
      * @param id the id of the lobby
      */
-    public void removeLobby(int id){
-}
+    public void removeLobby(int id) {
+        
+    }
 
+    /**
+     * change the name of the lobby
+     * @param id the id of the lobby
+     * @param newName the new name of the lobby
+     */
     public void changeLobbyName(int id, String newName) {
         for (Lobby l : lobbyList) {
             if (l.getID() == id) {
@@ -57,6 +66,12 @@ public class ServerReceiver {
         }
     }
 
+    /**
+     * join a lobby
+     * add a player to the lobby
+     * @param id the id of the lobby
+     * @param player the player which will join the lobby
+     */
     public void joinLobby(int id, IPlayer player) {
 
     }
